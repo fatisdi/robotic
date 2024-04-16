1ae06fd20ba980947734f49858f4d56bb42a5b33
fateme sadeqi <140097873+fatisdi@users.noreply.github.com>
2024-04-16 22:25:05 +0330

    Add files via upload

--- sketch.ino
+++ sketch.ino
@@ -1,45 +1,139 @@
-// Define the pins for each segment (a to g) and the common cathode pin
-const int segmentPins[8] = {2, 3, 4, 5, 6, 7, 8};
-const int commonCathodePin = 9;
-
-const byte numbers[11] = {
-  B11111100, // 0
-  B01100000, // 1
-  B11011010, // 2
-  B11110010, // 3
-  B01100110, // 4
-  B10110110, // 5
-  B10111110, // 6
-  B11100000, // 7
-  B11111110, // 8
-  B11110110  // 9
-};
-
 void setup() {
-  for (int i = 0; i < 7; i++) {
-    pinMode(segmentPins[i], OUTPUT);
-  }
+  // put your setup code here, to run once:
+pinMode(LED_BUILTIN , OUTPUT);
+pinMode(0 , OUTPUT);
+pinMode(1 , OUTPUT);
+pinMode(2 , OUTPUT);
+pinMode(3 , OUTPUT);
+pinMode(4 , OUTPUT);
+pinMode(5 , OUTPUT);
+pinMode(6 , OUTPUT);
+pinMode(7 , OUTPUT);
+pinMode(8 , OUTPUT);
+pinMode(9 , OUTPUT);
 
-  pinMode(commonCathodePin, OUTPUT);
 }
 
 void loop() {
-  for (int i = 0; i < 10; i++) 
-  {
-    displayNumber(i);
-    delay(1000); 
-  }
-}
+  // put your main code here, to run repeatedly:
+  // adad yek 
+  delay(1000);
+digitalWrite(0, HIGH);
+digitalWrite(1, LOW);
+digitalWrite(2, LOW);
+digitalWrite(3, LOW);
+digitalWrite(4, LOW);
+digitalWrite(5, LOW);
+digitalWrite(6, LOW);
+digitalWrite(7, HIGH);
+digitalWrite(8, HIGH);
+digitalWrite(9, HIGH);
+delay(1000);
 
-void displayNumber(int num) {
-  for (int i = 0; i < 8; i++) {
-    digitalWrite(segmentPins[i], HIGH);
-  }
+digitalWrite(0, HIGH);
+digitalWrite(1, HIGH);
+digitalWrite(2, LOW);
+digitalWrite(3, LOW);
+digitalWrite(4, HIGH);
+digitalWrite(5, HIGH);
+digitalWrite(6, HIGH);
+digitalWrite(7, HIGH);
+digitalWrite(8, HIGH);
+digitalWrite(9, HIGH);
+delay(1000);
 
-  for (int i = 0; i < 8; i++) {
-    if (bitRead(numbers[num], i) == LOW) {
-      digitalWrite(segmentPins[7-i], LOW);
-    }
-  }
+digitalWrite(0, HIGH);
+digitalWrite(1, LOW);
+digitalWrite(2, LOW);
+digitalWrite(3, HIGH);
+digitalWrite(4, LOW);
+digitalWrite(5, LOW);
+digitalWrite(6, HIGH);
+digitalWrite(7, LOW);
+digitalWrite(8, HIGH);
+digitalWrite(9, HIGH);
+delay(1000);
+
+digitalWrite(0, HIGH);
+digitalWrite(1, LOW);
+digitalWrite(2, LOW);
+digitalWrite(3, LOW);
+digitalWrite(4, LOW);
+digitalWrite(5, HIGH);
+digitalWrite(6, HIGH);
+digitalWrite(7, LOW);
+digitalWrite(8, HIGH);
+digitalWrite(9, HIGH);
+delay(1000);
+
+digitalWrite(0, HIGH);
+digitalWrite(1, HIGH);
+digitalWrite(2, LOW);
+digitalWrite(3, LOW);
+digitalWrite(4, HIGH);
+digitalWrite(5, HIGH);
+digitalWrite(6, LOW);
+digitalWrite(7, LOW);
+digitalWrite(8, HIGH);
+digitalWrite(9, HIGH);
+delay(1000);
+
+digitalWrite(0, HIGH);
+digitalWrite(1, LOW);
+digitalWrite(2, HIGH);
+digitalWrite(3, LOW);
+digitalWrite(4, LOW);
+digitalWrite(5, HIGH);
+digitalWrite(6, LOW);
+digitalWrite(7, LOW);
+digitalWrite(8, HIGH);
+digitalWrite(9, HIGH);
+delay(1000);
+
+digitalWrite(0, HIGH);
+digitalWrite(1, LOW);
+digitalWrite(2, HIGH);
+digitalWrite(3, LOW);
+digitalWrite(4, LOW);
+digitalWrite(5, LOW);
+digitalWrite(6, LOW);
+digitalWrite(7, LOW);
+digitalWrite(8, HIGH);
+digitalWrite(9, HIGH);
+delay(1000);
+
+digitalWrite(0, HIGH);
+digitalWrite(1, LOW);
+digitalWrite(2, LOW);
+digitalWrite(3, LOW);
+digitalWrite(4, HIGH);
+digitalWrite(5, HIGH);
+digitalWrite(6, HIGH);
+digitalWrite(7, HIGH);
+digitalWrite(8, HIGH);
+digitalWrite(9, HIGH);
+delay(1000);
+
+digitalWrite(0, HIGH);
+digitalWrite(1, LOW);
+digitalWrite(2, LOW);
+digitalWrite(3, LOW);
+digitalWrite(4, LOW);
+digitalWrite(5, LOW);
+digitalWrite(6, LOW);
+digitalWrite(7, LOW);
+digitalWrite(8, HIGH);
+digitalWrite(9, HIGH);
+delay(1000);
+digitalWrite(0, HIGH);
+digitalWrite(1, LOW);
+digitalWrite(2, LOW);
+digitalWrite(3, LOW);
+digitalWrite(4, LOW);
+digitalWrite(5, HIGH);
+digitalWrite(6, LOW);
+digitalWrite(7, LOW);
+digitalWrite(8, HIGH);
+digitalWrite(9, HIGH);
 
 }
