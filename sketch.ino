a66e6ecf16aade7a03cee5c9222ab0f4e188d044
fateme sadeqi <140097873+fatisdi@users.noreply.github.com>
2024-04-16 22:44:21 +0330

    Add files via upload

--- sketch.ino
+++ sketch.ino
@@ -2,6 +2,7 @@
 const int segmentPins[8] = {2, 3, 4, 5, 6, 7, 8};
 const int commonCathodePin = 9;
 
+// Define the numbers to display on the seven-segment display
 const byte numbers[11] = {
   B11111100, // 0
   B01100000, // 1
@@ -16,30 +17,43 @@ const byte numbers[11] = {
 };
 
 void setup() {
+  // Set segment pins as outputs
   for (int i = 0; i < 7; i++) {
     pinMode(segmentPins[i], OUTPUT);
   }
 
+  // Set common cathode pin as output
   pinMode(commonCathodePin, OUTPUT);
 }
 
 void loop() {
+  // Count from 0 to 9
   for (int i = 0; i < 10; i++) 
   {
     displayNumber(i);
-    delay(1000); 
+   // while(1);
+    delay(1000); // Display each number for 1 second
+
+   // for (int j = 0; j < 8; j++) {
+   // digitalWrite(segmentPins[j], LOW);
+  //}
   }
 }
 
+// Function to display a digit on the seven-segment display
 void displayNumber(int num) {
+  // Turn off all segments
   for (int i = 0; i < 8; i++) {
     digitalWrite(segmentPins[i], HIGH);
   }
 
+  // Activate segments based on the number to be displayed
   for (int i = 0; i < 8; i++) {
     if (bitRead(numbers[num], i) == LOW) {
       digitalWrite(segmentPins[7-i], LOW);
     }
   }
 
+  // Turn on the common cathode to display the digit
+ // digitalWrite(commonCathodePin, LOW);
 }
